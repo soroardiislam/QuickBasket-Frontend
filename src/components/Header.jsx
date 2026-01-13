38834c4b77ec4dd0be394afdb35d5dc4848fd054
Md Soroardi Islam <mdsoroardi602828@gmail.com>
2026-01-13 21:56:12 +0600

    header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,107 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from '../assets/logo.png'
+import Search from './Search'
+import { Link, useLocation,useNavigate } from 'react-router-dom'
+import { FaRegCircleUser } from "react-icons/fa6";
+import useMobile from '../hooks/useMobile';
+import { BsCart4 } from "react-icons/bs";
+import { useSelector } from 'react-redux';
+import { GoTriangleDown, GoTriangleUp  } from "react-icons/go";
+import UserMenu from './UserMenu';
+import { DisplayPriceInRupees } from '../utils/DisplayPriceInRupees';
+import { useGlobalContext } from '../provider/GlobalProvider';
+import DisplayCartItem from './DisplayCartItem';
 
-function Header() {
+const Header = () => {
+    const [ isMobile ] = useMobile()
+    const location = useLocation()
+    const isSearchPage = location.pathname === "/search"
+    const navigate = useNavigate()
+    const user = useSelector((state)=> state?.user)
+    const [openUserMenu,setOpenUserMenu] = useState(false)
+    const cartItem = useSelector(state => state.cartItem.cart)
+    // const [totalPrice,setTotalPrice] = useState(0)
+    // const [totalQty,setTotalQty] = useState(0)
+    const { totalPrice, totalQty} = useGlobalContext()
+    const [openCartSection,setOpenCartSection] = useState(false)
+ 
+    const redirectToLoginPage = ()=>{
+        navigate("/login")
+    }
+
+    const handleCloseUserMenu = ()=>{
+        setOpenUserMenu(false)
+    }
+
+    const handleMobileUser = ()=>{
+        if(!user._id){
+            navigate("/login")
+            return
+        }
+
+        navigate("/user")
+    }
+
+    //total item and total price
+    // useEffect(()=>{
+    //     const qty = cartItem.reduce((preve,curr)=>{
+    //         return preve + curr.quantity
+    //     },0)
+    //     setTotalQty(qty)
+        
+    //     const tPrice = cartItem.reduce((preve,curr)=>{
+    //         return preve + (curr.productId.price * curr.quantity)
+    //     },0)
+    //     setTotalPrice(tPrice)
+
+    // },[cartItem])
+
   return (
-    <div>header</div>
+    <header className='h-24 lg:h-20 lg:shadow-md sticky top-0 z-40 flex flex-col justify-center gap-1 bg-white'>
+        {
+            !(isSearchPage && isMobile) && (
+                <div className='container mx-auto flex items-center px-2 justify-between'>
+                                {/**logo */}
+                                <div className='h-full'>
+                                    <Link to={"/"} className='h-full flex justify-center items-center'>
+                                        <img 
+                                            src={logo}
+                                            width={170}
+                                            height={60}
+                                            alt='logo'
+                                            className='hidden lg:block'
+                                        />
+                                        <img 
+                                            src={logo}
+                                            width={120}
+                                            height={60}
+                                            alt='logo'
+                                            className='lg:hidden'
+                                        />
+                                    </Link>
+                                </div>
+
+                                {/**Search */}
+                                <div className='hidden lg:block'>
+                                    <Search/>
+                                </div>
+
+
+                               
+                </div>
+            )
+        }
+        
+        <div className='container mx-auto px-2 lg:hidden'>
+            <Search/>
+        </div>
+
+        {
+            openCartSection && (
+                <DisplayCartItem close={()=>setOpenCartSection(false)}/>
+            )
+        }
+    </header>
   )
 }
 
